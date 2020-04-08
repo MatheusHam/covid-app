de32d544be6aff98b8238da4afdc648a2db037c9
Matheus Rodrigues Rugolo <matheusrrugolo@gmail.com>
2020-04-08 13:09:38 -0300

    Multiple countries

--- app.py
+++ app.py
@@ -5,11 +5,17 @@ import matplotlib.pyplot as plt
 
 def main():
     df = load_data()
-    page = st.sidebar.selectbox("Work In Progress", ['Homepage'])
+    page = st.sidebar.selectbox("Visualizações", ['Evolução dos casos'])
 
-    if page == 'Homepage':
-        country = st.selectbox("Choose a Country", df['Country/Region'].unique(), index=1)
-        create_graph(df, country)
+    if page == 'Evolução dos casos':
+        st.title('')
+        st.text('Compare o aumento de casos em relação a outros paises a partir\n do primeiro caso confirmado.')
+        st.text('Fonte: https://www.kaggle.com/imdevskp/corona-virus-report/data#')
+        st.text('Dados atualizados em 08/04/2020')
+        countries = [st.selectbox("Country - 1", df['Country/Region'].unique(), index=21),
+                     st.selectbox("Country - 2", df['Country/Region'].unique(), index=138),
+                     st.selectbox("Country - 3", df['Country/Region'].unique(), index=156)]
+        create_graph(df, countries)
 
 
 @st.cache
@@ -19,43 +25,26 @@ def load_data():
     return df
 
 
-def create_graph(df, country):
-    st.title(f'País de comparação: {country}')
-    st.text('Compare o aumento de casos no Brasil em relação a outros paises a partir\n do primeiro caso confirmado.')
-    st.text('Fonte: https://www.kaggle.com/imdevskp/corona-virus-report/data#')
-    st.text('Dados atualizados em 07/04/2020')
-
-    filter = (df['Country/Region'] == country) & (df['Confirmed'] > 0)
-    filter2 = (df['Country/Region'] == 'Brazil') & (df['Confirmed'] > 0)
-    dfcountry = df[filter].copy()
-    dfbrazil = df[filter2].copy()
-
-    qtd_dias_country = []
-    qtd_dias_brazil = []
-    for x in range(len(dfbrazil['Date'])):
-        qtd_dias_brazil.append(x)
-    for x in range(len(dfcountry['Date'])):
-        qtd_dias_country.append(x)
-
-    dfcountry.insert(0, "Dias", qtd_dias_country, True)
-    dfbrazil.insert(0, "Dias", qtd_dias_brazil, True)
-
+def create_graph(df, countries):
     fig, ax = plt.subplots()
-
-    day_c = dfcountry['Dias']
-    day_bra = dfbrazil['Dias']
-    confirmed_br = dfbrazil['Confirmed']
-    confirmed_c = dfcountry['Confirmed']
-
-    ax.plot(day_bra, confirmed_br, color="C0", label='Brasil')
-    ax.plot(day_c, confirmed_c, color="C1", label=f'{country}')
+    color = 0
+    for country in countries:
+        filter = (df['Country/Region'] == country) & (df['Confirmed'] > 0)
+        dfcountry = df[filter].copy()
+        qtd_dias_country = []
+        for x in range(len(dfcountry['Date'])):
+            qtd_dias_country.append(x)
+        dfcountry.insert(0, "Dias", qtd_dias_country, True)
+        day_c = dfcountry['Dias']
+        confirmed_c = dfcountry['Confirmed']
+        color += 1
+        color_text = 'C'+str(color)
+        ax.plot(day_c, confirmed_c, color=color_text, label=f'{country}')
 
     ax.legend()
     ax.set(xlabel='Dias Corridos', ylabel='Casos Confirmados', title='Evolução de casos')
     ax.grid()
     ax.set_yscale('log')
-
-    fig.savefig('graph')
 
     st.pyplot()
 
